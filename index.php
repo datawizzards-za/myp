32f52b607af68e3d756fa8cf77484764c3d34dde
Mabu <manailengmj@gmail.com>
2017-07-13 08:49:03 +0200

    Commit 2

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
     <head>
         <meta charset=utf-8>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pluton Theme by BraphBerry.com</title>
-        <!-- Load Roboto font -->
+        <title>MYP</title>
+        <!-- Load Roboto font 
         <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        -->
         <!-- Load css styles -->
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
         <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css" />
@@ -29,15 +30,15 @@
         <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114.png">
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72.png">
         <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57.png">
-        <link rel="shortcut icon" href="images/ico/favicon.ico">
+        <link rel="shortcut icon" href="img/logo_trans.png">
     </head>
     
     <body>
         <div class="navbar">
             <div class="navbar-inner">
                 <div class="container">
-                    <a href="#" class="brand">
-                        <img src="images/logo.png" width="120" height="40" alt="Logo" />
+                    <a href="./" class="brand">
+                        <img src="img/logo_bg.jpg" width="60" height="50" alt="Logo" />
                         <!-- This is website logo -->
                     </a>
                     <!-- Navigation button, visible on small resolution -->
@@ -48,9 +49,9 @@
                     <div class="nav-collapse collapse pull-right">
                         <ul class="nav" id="top-navigation">
                             <li class="active"><a href="#home">Home</a></li>
-                            <li><a href="#service">Services</a></li>
-                            <li><a href="#portfolio">Portfolio</a></li>
                             <li><a href="#about">About</a></li>
+                            <li><a href="#portfolio">Projects</a></li>
+                            <li><a href="#service">Services</a></li>
                             <li><a href="#clients">Clients</a></li>
                             <li><a href="#price">Price</a></li>
                             <li><a href="#contact">Contact</a></li>
